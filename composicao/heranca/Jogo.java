b6a36e5bba915ea827af56c6ec54e9cab8b946e4
Gustavo Maciel Jacinto <126967183+Gustavomjj@users.noreply.github.com>
2023-04-27 20:32:15 -0300

    Add files via upload

--- composicao/heranca/Jogo.java
+++ composicao/heranca/Jogo.java
@@ -1,22 +1,36 @@
 package oo.heranca;
 
 public class Jogo {
-
+	
 	public static void main(String[] args) {
 		
-		Jogador j1 = new Jogador();
-		j1.andar(Direcao.NORTE);
-		j1.andar(Direcao.NORTE);
-		j1.andar(Direcao.NORTE);
-		j1.andar(Direcao.NORTE);
-		j1.andar(Direcao.OESTE);
-		j1.andar(Direcao.OESTE);
-		j1.andar(Direcao.OESTE);
-		j1.andar(Direcao.OESTE);
-		j1.andar(Direcao.OESTE);
+		Heroi j1 = new Heroi();
+		j1.andar(Direcao.SUL);
+
+		Monstro j2 = new Monstro();
+		j2.andar(Direcao.OESTE);
+		j2.andar(Direcao.SUL);
 		
+		System.out.println(j1.vida);
+				
 		System.out.println(j1.x);
 		System.out.println(j1.y);
 		
+		System.out.println(j2.x);
+		System.out.println(j2.y);
+		
+		j2.atacar(j1);
+		
+		System.out.println(j1.vida);
+		System.out.println(j2.vida);
+		
+		j1.atacar(j2);
+		
+		System.out.println(j1.vida);
+		System.out.println(j2.vida);
+		
+		
+		
 	}
+	
 }
